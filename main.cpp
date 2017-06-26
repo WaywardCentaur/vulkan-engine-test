61629b603953801a1717a709c8a9168e19255db4
Ray <raymondandre95@gmail.com>
2017-06-26 00:26:30 -0500

    2nd commit

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@
 #include <vector>
 #include <cstring>
 #include <set>
+#include <fstream>
+
 
 const int WIDTH = 800;
 const int HEIGHT = 600;
@@ -87,6 +89,7 @@ public:
 			createLogicalDevice();
 			createSwapChain();
 			createImageViews();
+			createGraphicsPipeline();
 		}
 		//run till window is open
 		{
@@ -94,7 +97,6 @@ public:
 				glfwPollEvents();
 			}
 		}
-
 		// cleanup
 		{
 			for (VkImageView imageView : swapChainImageViews) {
@@ -173,7 +175,6 @@ private:
 	}
 
 	/////////////////////////////////////////////////debug//////////////////////////////////
-
 
 	void setupDebugCallback() {
 		if (!enableValidationLayers) return;
@@ -364,8 +365,62 @@ private:
 
 	}
 
+	////////////////////////////////////////////////Pipe Lines///////////////////////////////////////////
 
-	
+	void createGraphicsPipeline() {
+		auto vertShaderCode = readFile("shaders/vert.spv");
+		auto fragShaderCode = readFile("shaders/frag.spv");
+
+		VkShaderModule vertShaderModule;
+		VkShaderModule fragShaderModule;
+
+		vertShaderModule = createShaderModule(vertShaderCode);
+		fragShaderModule = createShaderModule(fragShaderCode);
+
+		VkPipelineShaderStageCreateInfo vertShaderStageInfo = {};
+		vertShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+		vertShaderStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
+		vertShaderStageInfo.module = vertShaderModule;
+		vertShaderStageInfo.pName = "main";
+
+		VkPipelineShaderStageCreateInfo fragShaderStageInfo = {};
+		fragShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+		fragShaderStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+		fragShaderStageInfo.module = fragShaderModule;
+		fragShaderStageInfo.pName = "main";
+
+		VkPipelineShaderStageCreateInfo shaderStages[] = { vertShaderStageInfo, fragShaderStageInfo };
+
+
+		VkPipelineVertexInputStateCreateInfo vertexInputInfo = {};
+		vertexInputInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+		vertexInputInfo.vertexBindingDescriptionCount = 0;
+		vertexInputInfo.pVertexBindingDescriptions = nullptr; // Optional
+		vertexInputInfo.vertexAttributeDescriptionCount = 0;
+		vertexInputInfo.pVertexAttributeDescriptions = nullptr; // Optional
+
+		vkDestroyShaderModule(device, fragShaderModule, nullptr);
+		vkDestroyShaderModule(device, vertShaderModule, nullptr);
+	}
+
+
+	////////////////////////////////////////////////Shader Stuff//////////////////////////////////////////
+
+	VkShaderModule createShaderModule(const std::vector<char>& code) {
+		// vkShaderModule struct
+		VkShaderModuleCreateInfo createInfo = {};
+		createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+		createInfo.codeSize = code.size();
+		createInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
+
+		VkShaderModule shaderModule;
+		if (vkCreateShaderModule(device, &createInfo, nullptr, &shaderModule) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create shader module!");
+		}
+
+		return shaderModule;
+	}
+
 
 	////////////////////////////////////////////////GPU Compatibility stuff///////////////////////////////
 
@@ -563,15 +618,25 @@ private:
 	}
 	
 
-	
 
-	///////////////////////////////////////////////////CleanUp//////////////////////////////
-	void cleanup() {
 
-		
+
+	/////////////////////////////////////File read/////////////////////////////////////////
+	static std::vector<char> readFile(const std::string& filename) {
+		std::ifstream file(filename, std::ios::ate | std::ios::binary);
+
+		if (!file.is_open()) {
+			throw std::runtime_error("failed to open file!");
+		}
+		size_t fileSize = (size_t)file.tellg();
+		std::vector<char> buffer(fileSize);
+		file.seekg(0);
+		file.read(buffer.data(), fileSize);
+		file.close();
+
+		return buffer;
 	}
 
-	/////////////////////////////////////////////////////////////////////////
 };
 
 int main() {
